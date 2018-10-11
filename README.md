d6300ba98c6ccd29e393a1ea6dbeea32223dd7f4
osdevisnot <osdevisnot@gmail.com>
2018-10-10 23:43:31 -0700

    Initial commit from Create React App

---
+++ README.md
@@ -0,0 +1,13 @@
+# starter-react
+
+This project is a starter for CRA based applications. To use this starter, ensure you have `degit` installed.
+
+```
+yarn global add degit
+```
+
+To create a new project based on this template:
+
+```
+degit osdevisnot/starter-react my-app && $_ && node setup.js
+```
---
+++ setup.js
@@ -0,0 +1,57 @@
+/**
+ * Simple setup script to be executed after degit finished cloning
+ * Note: Shell commands, Writing files manually should be avoided.
+ */
+const path = require('path')
+const fs = require('fs')
+const sync = require('child_process').execSync
+
+/**
+ * Last portion of working directory...
+ */
+const cwd = path.join(__dirname)
+const name = cwd.split(path.sep).pop()
+
+const fromRoot = file => path.join(cwd, file)
+
+/**
+ * Write a README.md file
+ */
+fs.writeFileSync(
+  fromRoot('README.md'),
+  `#${name}\n\nThis project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).\n\n`,
+  'utf-8'
+)
+
+/**
+ * ReWrite a package.json file
+ * Change package name and remove devDependencies while re-writing
+ */
+const pkg = require('./package.json')
+pkg.name = name
+const deps = Object.keys(pkg.dependencies).join(' ')
+const devDeps = Object.keys(pkg.devDependencies).join(' ')
+delete pkg.dependencies
+delete pkg.devDependencies
+fs.writeFileSync(fromRoot('package.json'), JSON.stringify(pkg, null, '  ') + '\n', 'utf-8')
+
+/**
+ * Remove Files and Self destruct
+ */
+const files = ['yarn.lock', 'setup.js', '.gitignore', '.prettierrc']
+files.forEach(file => fs.unlinkSync(fromRoot(file)))
+
+/**
+ * Add latest devDependencies and initialize git repo
+ */
+const commands = [
+  `yarn add ${deps}`,
+  `yarn add --dev ${devDeps}`,
+  'git init',
+  'git add .',
+  'git commit -am "first commit"'
+]
+commands.forEach(command => {
+  console.log(`----- Executing Command -----> ${command}`)
+  sync(command, { stdio: [0, 1, 2] })
+})
